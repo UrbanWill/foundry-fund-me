c82d5c9e7de6d66a302092d0406b183467b7ae4c
ApeLoko <apeloko.dev@gmail.com>
2023-09-28 13:01:20 +0700

    chore: deploy MockV3Aggregator for local testing

--- script/HelperCoinfig.s.sol
+++ script/HelperCoinfig.s.sol
@@ -2,9 +2,13 @@
 pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     struct NetworkConfig {
         address priceFeed;
@@ -31,5 +35,18 @@ contract HelperConfig is Script {
     function getOrCreateAnvilEthConfig()
         public
         returns (NetworkConfig memory anvilNetworkConfig)
-    {}
+    {
+        // Check to see if we set an active network config
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        anvilNetworkConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+    }
 }
