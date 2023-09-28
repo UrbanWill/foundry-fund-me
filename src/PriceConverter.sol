762871b3b6d2b58559e6eb0caf811f6fb4681d7c
ApeLoko <apeloko.dev@gmail.com>
2023-09-28 11:10:27 +0700

    refactor: pass Aggregator address in constructor

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getPrice() internal view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
+    function getPrice(
+        AggregatorV3Interface _priceFeed
+    ) internal view returns (uint256) {
+        (, int256 answer, , , ) = _priceFeed.latestRoundData();
         // ETH/USD rate in 18 digit
         return uint256(answer * 10000000000);
     }
 
     function getConversionRate(
-        uint256 ethAmount
+        uint256 ethAmount,
+        AggregatorV3Interface _priceFeed
     ) internal view returns (uint256) {
-        uint256 ethPrice = getPrice();
+        uint256 ethPrice = getPrice(_priceFeed);
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         return ethAmountInUsd;
     }
